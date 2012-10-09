08cbde9a04dfd72a525c37f12f06329e30284d94
Hui Hu <huh@vmware.com>
2012-10-09 12:18:37 +0800

    Correct path of hadoop log dir; Remove an unused file.

--- cookbooks/hadoop_cluster/recipes/hadoop_dir_perms.rb
+++ cookbooks/hadoop_cluster/recipes/hadoop_dir_perms.rb
@@ -5,4 +5,4 @@ dfs_name_dirs.each{      |dir| ensure_hadoop_owns_hadoop_dirs(dir, 302, "0700") 
 dfs_data_dirs.each{      |dir| ensure_hadoop_owns_hadoop_dirs(dir, 302, "0755") }
 fs_checkpoint_dirs.each{ |dir| ensure_hadoop_owns_hadoop_dirs(dir, 302, "0700") }
 mapred_local_dirs.each{  |dir| ensure_hadoop_owns_hadoop_dirs(dir, 303, "0755") }
-ensure_hadoop_owns_hadoop_dirs('/mnt/hadoop/logs',                 302, "0775")
+ensure_hadoop_owns_hadoop_dirs(local_hadoop_log_dir, 302, "0775")
