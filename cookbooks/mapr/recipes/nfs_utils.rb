2737df9c029bee15bc6f2e1782e866b1aa33f19e
Hui Hu <huh@vmware.com>
2013-04-22 10:54:53 +0800

    For MapR, install rbcbind on CentOS 6.x and portmap on CentOS 5.x. rbcbind and portmap provide the same functionality.

--- cookbooks/mapr/recipes/nfs_utils.rb
+++ cookbooks/mapr/recipes/nfs_utils.rb
@@ -13,8 +13,23 @@
 #   limitations under the License.
 #
 
+# install NFS utils
 package 'nfs-utils'
-package 'portmap'
-service 'portmap' do
+
+# install RPC service
+case node['platform']
+when "redhat", "centos", "scientific", "oracle"
+  if node['platform_version'].to_f >= 6.0
+    pkg_name = 'rpcbind'
+    svc_name = 'rpcbind'
+  else
+    pkg_name = 'portmap'
+    svc_name = 'portmap'
+  end
+end
+
+package pkg_name
+service svc_name do
   action :start
 end
+
