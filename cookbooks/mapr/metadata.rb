695904728051b878b613037c61aaa86da1ea41b5
Hui Hu <huh@vmware.com>
2013-05-23 17:32:21 +0800

    Add Portions Copyright notice

--- cookbooks/mapr/metadata.rb
+++ cookbooks/mapr/metadata.rb
@@ -3,7 +3,7 @@ maintainer_email  "serengeti-dev@googlegroups.com"
 license           "Apache 2.0"
 description       "MapR cookbook"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.txt'))
-version           "0.1.0"
+version           "0.2.0"
 
 depends           "hadoop_common"
 depends           "cluster_service_discovery"
